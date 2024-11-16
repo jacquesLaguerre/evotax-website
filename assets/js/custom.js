c01261532ab9ba1d437690229645b7f139f161b0
jacquesLaguerre <105319616+jacquesLaguerre@users.noreply.github.com>
2024-11-16 10:06:58 -0600

    lag/long changed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,8 +6,8 @@ var resizeId;
 $(document).ready(function($) {
     "use strict";
 
-    var latitude = 34.038405;
-    var longitude = -117.946944;
+    var latitude = 38.649;
+    var longitude = -90.327;
     var markerImage = "assets/img/map-marker.png";
     var mapElement = "map";
     var mapStyle = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#dbdbdb"},{"visibility":"on"}]}];
